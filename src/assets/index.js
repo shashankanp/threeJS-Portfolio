6374cc25ba27c4c0728bc9b8e7eed2d5d9d14835
shashankanp <shashanknp02@gmail.com>
2023-06-08 12:48:55 +0530

    Testimonlas to be done

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,19 +8,19 @@ import lightning from "./lightning.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+// import css from "./tech/css.png";
+// import docker from "./tech/docker.png";
+// import figma from "./tech/figma.png";
+// import git from "./tech/git.png";
+// import html from "./tech/html.png";
+// import javascript from "./tech/javascript.png";
+// import mongodb from "./tech/mongodb.png";
+// import nodejs from "./tech/nodejs.png";
+// import reactjs from "./tech/reactjs.png";
+// import redux from "./tech/redux.png";
+// import tailwind from "./tech/tailwind.png";
+// import typescript from "./tech/typescript.png";
+// import threejs from "./tech/threejs.svg";
 
 import sentinal from "./company/sentinal.png";
 import kaamik from "./company/kaamik.png";
@@ -41,19 +41,19 @@ export {
   lightning,
   menu,
   close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
+  // css,
+  // docker,
+  // figma,
+  // git,
+  // html,
+  // javascript,
+  // mongodb,
+  // nodejs,
+  // reactjs,
+  // redux,
+  // tailwind,
+  // typescript,
+  // threejs,
   kaamik,
   bhive,
   nte,
