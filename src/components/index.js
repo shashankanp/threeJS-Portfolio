2745f5a18f900277b9c08a4e0bfd10e2246959f3
shashankanp <shashanknp02@gmail.com>
2023-06-12 11:31:58 +0530

    Testimonials to be done v2

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,4 @@
-import {
-  EarthCanvas,
-  BallCanvas,
-  ComputersCanvas,
-  StarsCanvas,
-} from "./canvas";
+import { EarthCanvas, ComputersCanvas, StarsCanvas } from "./canvas";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
@@ -21,7 +16,6 @@ export {
   Feedbacks,
   Contact,
   EarthCanvas,
-  BallCanvas,
   ComputersCanvas,
   StarsCanvas,
 };
